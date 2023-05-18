8afee5e6dfd2f1fad871add6477263061bb7693a
jordansbenjamin <14327@coderacademy.edu.au>
2023-05-18 14:41:11 +1000

    Added file entry

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import json
 import os.path
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 # Allows us to securely send messages back and forth to the user
 # For the time being because we're in development we can provide any string for the key but when in production create a strong randomised key
-app.secret_key = 'wqkbhcwklqfbwklebh'
+# app.secret_key = 'wqkbhcwklqfbwklebh'
 
 # Homepage/base url route is generally indicated with a /
 @app.route('/')
@@ -31,7 +32,17 @@ def your_url():
         if request.form['code'] in urls.keys():
             # Displays message to users when a short name has already been taken
             flash('That short name has already been taken. Please select another name.')
-            return redirect(url_for('home'))    
+            return redirect(url_for('home'))
+        
+        # To check if this is a file or URL
+        if 'url' in request.form.keys():
+            urls[request.form['code']] = {'url': request.form['url']}
+        else:
+            f = request.files['file']
+            full_name = request.form['code'] + secure_filename(f.filename)
+            # Decide where to save the file
+            f.save('/Users/jordanbenjamin/Courses/Bootcamp/CoderAcademy/T1/Week10/url-shortener_FlaskApp/images_db/' + full_name)
+            urls[request.form['code']] = {'file': full_name}
 
         urls[request.form['code']] = {'url': request.form['url']}
         with open('urls.json', 'w') as url_file:
