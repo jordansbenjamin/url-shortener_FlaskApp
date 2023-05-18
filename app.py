ac40b540da82eaa2864ad304d16f06aea5e001fc
jordansbenjamin <14327@coderacademy.edu.au>
2023-05-18 13:13:17 +1000

    Added home html file, connected + tested jinja templating

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Homepage/base url route
+# Homepage/base url route is generally indicated with a /
 @app.route('/')
 def home():
-    return 'Hello Flask!'
+    # return 'Hello Flask!'
+    # instead of returning the string we can return the home.html file instead
+    # Using the render template function which is imported above
+    return render_template('home.html')
+    # return render_template('home.html', name='Jordan')
 
 # Name of route and function does not need to match
 @app.route('/about')
 def about():
     return 'This is a url shortener'
+
